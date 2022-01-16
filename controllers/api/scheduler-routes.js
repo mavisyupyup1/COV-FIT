25e754866faaee75da2c941f7931876b2ca4b95d
mavisyupyup1 <mavisyupyup1@github.com>
2022-01-15 19:17:55 -0500

    added my schedule page and post routes

--- controllers/api/scheduler-routes.js
+++ controllers/api/scheduler-routes.js
@@ -2,15 +2,17 @@ const router = require('express').Router();
 const { Schedule, User, Painting } = require('../../models');
 const withAuth = require('../../utils/auth');
 //get all scheduled item
-router.get('/',(req,res)=>{
-    console.log('===============')
+router.get('/',withAuth,(req,res)=>{
+    console.log('=============== ')
+    console.log(req.session.user_id)
     Schedule.findAll({
-        // where:{
-        //     user_id:req.session.user_id
-        // },
+        where:{
+            user_id:req.session.user_id
+        },
         attributes:[
             'id',
             'painting_id', 
+            'user_id',
             'start',  
         ],
         include:[
@@ -19,28 +21,31 @@ router.get('/',(req,res)=>{
                 attributes:['id']
             }
         ]
-    }).then(dbScheduleData=>{
-        res.json(dbScheduleData)
+    })
+    .then(dbScheduleData=>{
+        const schedules = dbScheduleData.map(schedule=>schedule.map({plain:true}));
+        res.render('myschedule',{schedules,loggedIn:true})
     }).catch(err=>{
         console.log(err);
         res.status(500).json(err)
     })
 })
 
-router.post("/", (req, res)=>{
+router.post("/",(req, res)=>{
         //console.log(req.body);
 if(req.session){
     Schedule.create({ 
-        use_id:req.session.use_id,
+        user_id:req.session.user_id,
         painting_id:req.body.painting_id,
         start:req.body.date
     })
-        .then(dbScheduleData=> {
-          res.json(dbScheduleData);
-        ;})
-   
+    .then(dbScheduleData=>res.json(dbScheduleData))
+   .catch(err=>{
+       console.log(err);
+       res.status(400).json(err)
+   })
  }
-
 });
+
 
 module.exports=router;
