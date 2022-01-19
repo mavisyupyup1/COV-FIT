d9a7002fe2093cd20101481f1af6d49971c3acbe
anaqvi1994 <aa.naqvi@outlook.com>
2022-01-19 11:03:52 -0500

    edited login js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,50 @@
-const mysql = require(`mysql`)
-const express = require(`express`)
-const session = require(`session`)
-const bodyParser = require(`body-parser`)
-const path = require(`path`)
-const handlebars = require(`handlebars`)
-//Create and add CovFit database here when complete
+const loginFormHandler = async (event) => {
+  event.preventDefault();
 
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
