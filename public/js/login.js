355af8cf3c44b29bb2866fd2b0297b5bef7ce582
anaqvi1994 <aa.naqvi@outlook.com>
2022-01-19 10:16:14 -0500

    local changes

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,6 @@ const express = require(`express`)
 const session = require(`session`)
 const bodyParser = require(`body-parser`)
 const path = require(`path`)
-
+const handlebars = require(`handlebars`)
 //Create and add CovFit database here when complete
 
